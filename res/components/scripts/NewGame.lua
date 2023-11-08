3d0d0cf7c47cc9e444fd3e2c1e1b5bf2bbc450d2
Enem-20 <firewall606@gmail.com>
2023-11-08 21:28:16 +0300

    deleted legacy projects

--- res/components/scripts/NewGame.lua
+++
@@ -1,8 +0,0 @@
-function Awake()
-    Engine.WindowManager.GetCurrentWindow():GetUI("NewGame"):AddListener(NewGame)
-end
-
-function NewGame()
-    Engine.ResourceManager.loadScene("res/saves/GameObjects.json")
-    print("newGame")
-end
